377395a6f2a63a32ce06e71c5710e397a969bb29
zexwang <zexwang@tencent.com>
2023-01-06 17:51:30 +0800

    init project

---
+++ run_pigeon.sh
@@ -0,0 +1,12 @@
+# run_pigeon.sh
+
+
+mkdir -p android/src/main/java/com/example/flutter_pigeon_plugin
+
+flutter pub run pigeon \
+  --input pigeons/message.dart \
+  --dart_out lib/message.dart \
+  --objc_header_out ios/Classes/Message.h \
+  --objc_source_out ios/Classes/Message.m \
+  --java_out android/src/main/java/com/example/flutter_pigeon_plugin/Message.java \
+  --java_package "com.example.flutter_pigeon_plugin"
